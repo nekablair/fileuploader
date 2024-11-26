65826d7f0e5cf0f3c6f863ebcb4188a54d714b33
nekablair <ourblairfamily@gmail.com>
2024-11-26 16:12:02 -0600

    add upload functionality

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, jsonify, render_template
+from azure.storage.blob import BlobServiceClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+storage_name = os.getenv("AZURE_CONTAINER_NAME")
+
+app = Flask(__name__)
+
+app.config["UPLOAD_FOLDER"] = "uploads"
+app.config["ALLOWED_EXTENSIONS"] = {"txt", "pdf", "png", "jpg", "jpeg", "gif"}
+
+#azure storage config
+STORAGE_ACCOUNT_CONNECTION_STRING = connection_string
+
+#azure container name
+container_name = "photos"
+
+blob_service_client = BlobServiceClient.from_connection_string(STORAGE_ACCOUNT_CONNECTION_STRING)
+container_client = blob_service_client.get_container_client(container_name)
+
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in app.config["ALLOWED_EXTENSIONS"]
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return render_template("index.html")
+
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    if "file" not in request.files:
+        return jsonify({"error": "No file part"}), 400
+
+    file = request.files["file"]
+    if file.filename == "":
+        return jsonify({"error": "No selected file"}), 400
+    
+    try:
+        if file and allowed_file(file.filename):
+            #generate blob name
+            blob_name = file.filename
+
+            #upload file to azure blob storage
+            blob_client = container_client.get_blob_client(file.filename)
+            blob_client.upload_blob(file, overwrite=True)
+            return jsonify({"message": f"File {file.filename} uploaded successfully!"}), 200
+        
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+if __name__ == "__main__":
+    app.run(debug=True)
